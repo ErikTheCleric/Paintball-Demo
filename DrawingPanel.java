6918e5f7b8c0ef6232c2f9fc6e862732d1b78d94
ErikTheCleric <erikhale@outlook.com>
2018-02-01 14:29:35 -0600

    Update DrawingPanel.java

--- DrawingPanel.java
+++ DrawingPanel.java
@@ -1,21 +1,88 @@
+import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class drawingPanel extends JPanel{
+public class drawingPanel extends Canvas implements Runnable{
 	
-	player p1 = new player(1, Color.RED, 100, 100, 100);
+	player p1 = new player(1, Color.RED, 100, 100, 10);
+	public static int FPStrace = 20;
+	private Thread thread;
+	private boolean running = true; // should be false
 	
-	@Override
-	public void paint(Graphics g) {
-		Graphics2D g2d = (Graphics2D)g;
-		p1.draw(g2d);
-		this.repaint();
+	public drawingPanel() {
+		this.setPreferredSize(new Dimension(1000,600));
+		this.addKeyListener(p1);
 	}
 	
-	
-}
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+		Graphics2D g2d = (Graphics2D)g;
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 1000, 700);
+		p1.draw(g2d);
+		g.dispose();
+		bs.show();
+	}
 
+	public synchronized void start() {
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void tick() {}
+	
+	@Override
+	public void run() {
+		this.requestFocus();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick();
+				delta--;
+			}
+			if(running) {
+				this.repaint();
+			}
+			frames++;
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+			try {
+				Thread.sleep(FPStrace);
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}	
+	}
+}
