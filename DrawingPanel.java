7ef2016afa86d3a06810c6618b2200ff1045d08f
ErikTheCleric <erikhale@outlook.com>
2018-02-01 13:38:27 -0600

    Update DrawingPanel.java

--- DrawingPanel.java
+++ DrawingPanel.java
@@ -1,53 +1,21 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.RenderingHints;
-import java.util.LinkedList;
+import java.awt.Graphics2D;
 
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class DrawingPanel extends JPanel {
+public class drawingPanel extends JPanel{
 	
-	private LinkedList<DotCharacteristics> dots = new LinkedList<DotCharacteristics>();
-	private boolean turnOnLines = false, zFormation = false;
-	
-	private int[][] arrayPoints = new int[LineCharacteristics.limitOfPoints][2];
-	
-	public void addDots(DotCharacteristics newDots) {
-		dots.add(newDots);
-		this.repaint();
-		
-	}
-	
-	public void savePoints(int[][] holdingArray) {
-		arrayPoints = holdingArray;
-	}
-	
-	public void connectLinesZ() {
-		zFormation = true;
-		this.repaint();
-	}
-	
-	public void connectLines() {
-		System.out.println("DRAWING LINES");
-		turnOnLines = true;
-		this.setBackground(Color.RED);
-		this.repaint();
-	}
+	player p1 = new player(1, Color.RED, 100, 100, 100);
 	
 	@Override
 	public void paint(Graphics g) {
-		
-		LineCharacteristics df = new LineCharacteristics(arrayPoints);
-		if(turnOnLines == true) {
-			df.drawAllLines(g);
-			turnOnLines =! turnOnLines;
-		}
-		if(zFormation == true) {
-			zFormation = false;
-			df.drawPointToNetNumPoint(g);
-		}
-		for(DotCharacteristics r : dots) {
-			r.draw(g);
-		}
+		Graphics2D g2d = (Graphics2D)g;
+		p1.draw(g2d);
+		this.repaint();
 	}
+	
+	
 }
+
