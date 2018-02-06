af41ae318e66de0bd5d43ad9caf21f449a526aa6
ErikTheCleric <erikhale@outlook.com>
2018-02-06 13:43:38 -0600

    Collision and Key Association

--- Handler.java
+++ Handler.java
@@ -5,54 +5,51 @@ import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 public class Handler extends KeyAdapter{
-	public ArrayList <player> players;
+	public ArrayList <player> Userplayers;
 	
 	public Handler() {
-		players = new ArrayList<player>();
+		Userplayers = new ArrayList<player>();
 	}
 	
 	public void paint(Graphics g) {
-		for(player i : players) {
+		for(player i : Userplayers) {
 			i.paint(g);
 		}
 	}
 	
 	public void update() {
-		for(player i : players) {
-			i.update();
+		if(Userplayers.get(0).circlesTouching(Userplayers.get(0).getX(), Userplayers.get(0).getY(), Userplayers.get(0).getDiameter() / 2, Userplayers.get(1).getX(), Userplayers.get(1).getY(), Userplayers.get(1).getDiameter() / 2)) {
+			Color temp = Userplayers.get(0).getColor();
+			Userplayers.get(0).colorSet(Userplayers.get(1).getColor());
+			Userplayers.get(1).colorSet(temp);
+
+			Userplayers.get(0).oppVelX();
+			Userplayers.get(0).oppVelY();
+			Userplayers.get(1).oppVelX();
+			Userplayers.get(1).oppVelY();
+			
+			//Userplayers.get(0).setVelX(0);
+			//Userplayers.get(1).setVelX(0);
 		}
-		for(int i = 0; i < players.size() / 2; i++) {
-			for(int j = (players.size() / 2) - 1; j < players.size(); j++) {
-				if(i != j) {
-					if(players.get(i).intersect(players.get(j))) {
-						System.out.println("intersecting");
-						Color pre = players.get(i).getColor();
-						players.get(i).colorSet(players.get(j).getColor());
-						players.get(j).colorSet(pre);
-						
-						players.get(i).setVelX(0);
-						players.get(i).setVelX(0);
-						players.get(j).setVelY(0);
-						players.get(j).setVelY(0);	
-					}
-				}
-			}
+		for(player i : Userplayers) {
+			i.update();
+			//i.setVelX(0);
 		}
 	}
 	
 	public void add(player i) {
-		players.add(i);
+		Userplayers.add(i);
 	}
 	
 	public void remove(player i) {
-		players.remove(i);
+		Userplayers.remove(i);
 	}
 	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		super.keyPressed(e);
 		int code = e.getKeyCode();
-		for(player i : players) {
+		for(player i : Userplayers) {
 			if(i.getMovement() == movementOptions.WASD) {
 				if(code == KeyEvent.VK_W) {
 					//System.out.println("W");
@@ -96,7 +93,7 @@ public class Handler extends KeyAdapter{
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		for(player i : players) {
+		for(player i : Userplayers) {
 			i.setVelX(0);
 			i.setVelX(0);
 			i.setVelY(0);
